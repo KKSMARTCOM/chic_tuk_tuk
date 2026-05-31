b2aedf1440679f2c35d69a6514cfc2dbc57ea89e
Arso991 <arsogn991@gmail.com>
2026-05-31 23:23:41 +0100

    Suppression des seeders obsolètes et ajout d'un fichier .dockerignore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            ConductorSeeder::class,
-            ZoneSeeder::class,
-            TarificationSeeder::class,
-            NotificationSeeder::class
         ]);
     }
 }
